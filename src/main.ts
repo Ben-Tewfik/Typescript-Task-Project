25a2b0ee8d356c6a6f1c7c2b76e8954ae859d775
benarba mohammed <tewfikhotspur@gmail.com>
2026-04-14 22:25:44 +0100

    build basic html structure and start with task logic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
+const form = document.querySelector<HTMLFormElement>(".form")!;
+const tasksList = document.querySelector<HTMLUListElement>(".list");
+const taskInput = document.querySelector<HTMLInputElement>(".task");
 
+type Task = {
+  description: string;
+  isCompleted: boolean;
+};
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let task = taskInput?.value;
+  if (task) {
+    // remove this console later
+    console.log(task);
+    return task;
+  }
+  alert("You did not enter anything");
+});
